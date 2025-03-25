7379b39c4f9a50ff31162e6b6149e5fdab2a0688
Grodoz1k <rbroyles1k@gmail.com>
2025-03-25 12:51:23 -0500

    Part 1 of Space Shooter

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.5f;
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private float _fireRate = 0.15f;
+    private float _canFire = -1f;
+    [SerializeField] private int _lives = 3;
+    private SpawnManager _spawnManager;
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        if (_spawnManager == null)
+        {
+            Debug.LogError("Spawn Manager is NULL!");
+        }
+    }
+    void Update()
+    {
+        Movement();
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.5f, 0);
+        }
+        if (transform.position.x >= 9.48f)
+        {
+            transform.position = new Vector3(9.48f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -9.48f)
+        {
+            transform.position = new Vector3(-9.48f, transform.position.y, 0);
+        }
+    }
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+    }
+    
+    public void Damage()
+    {
+        _lives -= 1;
+        if (_lives < 1)
+        {
+            _spawnManager.OnPlayerDeath();
+            Destroy(this.gameObject);
+        }
+    }
+}
